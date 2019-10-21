0e200670b24edee1df77ef85c041f38dbe2f9b0f
Misty <ms.misty.garcia@gmail.com>
2019-10-20 22:35:52 -0500

    added reidplot

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from math import sqrt
 from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
 from sklearn.linear_model import LinearRegression
+
+import statsmodels.api as sm
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 def compute_baseline(y):
     return np.array([y.mean()]*len(y))
@@ -31,6 +35,9 @@ def plot_linear_model(actuals, lm, baseline):
     .pipe((sns.relplot, 'data'), x='actual', y='prediction', hue='model')
     return plot
 
+def plot_residuals(X_train, y_train):
+    return sns.residplot(X_train, y_train)
+
 def plot_regression(x,y):
     res = sm.OLS(y, x).fit()
     prstd, iv_l, iv_u = wls_prediction_std(res)
@@ -38,9 +45,8 @@ def plot_regression(x,y):
     fig, ax = plt.subplots(figsize=(8,6))
 
     ax.plot(x, y, 'o', label="data")
-    #ax.plot(x, y, 'b-', label="True")
     ax.plot(x, res.fittedvalues, 'r--.', label="OLS")
-    ax.plot(x, iv_u, 'r--')
-    ax.plot(x, iv_l, 'r--')
+    ax.plot(x, iv_u, 'g--',label='97.5% Confidence Level')
+    ax.plot(x, iv_l, 'b--',label='2.5% Confidence Level')
     ax.legend(loc='best');
     plt.show()
